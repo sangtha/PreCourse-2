cd120e0eecb7dfe7ea71f6e82de17004c1895bfb
Sangeetha Chandrashekar <ch.sangtha@gmail.com>
2024-06-17 21:24:36 -0400

    precourse 2 ex1 and ex2 done

--- Exercise_1.js
+++ Exercise_1.js
@@ -1,7 +1,17 @@
 class BinarySearch {
      // Returns index of x if it is present in arr[l.. r], else return -1
-    function binarySearch(arr, l, r, x) {
-​
+    binarySearch(arr, l, r, x) {
+        
+        while(l <=r ){
+            let mid = parseInt((l+r)/2);
+        if(arr[mid] === x )
+            return mid;
+        else if(arr[mid]> x)
+            r = mid - 1;
+        else if(arr[mid] < x)
+            l = mid + 1;
+        }
+        return -1;
     }
 }
 // Driver method to test above
--- Exercise_2.js
+++ Exercise_2.js
@@ -1,31 +1,49 @@
 class QuickSort {
-​
+
       /* This function takes last element as pivot,
            places the pivot element at its correct
            position in sorted array, and places all
            smaller (smaller than pivot) to left of
            pivot and all greater elements to right
            of pivot */
-​
-    function swap(arr, i, j) {
+
+    swap(arr, i, j) {
         //Your code here
+        let tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
-​
-    function partition(arr, low, high) {
+
+    partition(arr, low, high) {
         //Write code here for Partition and Swap
+        let pivot = arr[high];
+        let i = low - 1;
+        for(let j = low; j <= high-1; j++){
+            if(arr[j]< pivot){
+                i++;
+                this.swap(arr, i, j);
+            }
+        }
+        this.swap(arr, i+1, high);
+        return i+1;
     }
-​
+
      /* The main function that implements QuickSort()
           arr[] --> Array to be sorted,
           low  --> Starting index,
           high  --> Ending index */
-    function sort(arr, low, high) {
+    sort(arr, low, high) {
              // Recursively sort elements before
              // partition and after partition
+        if(low >= high)
+            return arr;
+        let p = this.partition(arr, low, high);
+        this.sort(arr, p+1, high); 
+        this.sort(arr, low, p-1);
     }
-​
+
       /* A utility function to print array of size n */
-    function printArray(arr) {
+    printArray(arr) {
         let n = arr.length;
         for (let i = 0; i < n; ++i)
             console.log(arr[i] + " ");
